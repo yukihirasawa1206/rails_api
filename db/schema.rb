bac3b90ed9859cb16c54bc7bb4a4bd0bd58eb354
yukihirasawa1206 <h_x04yjr0@yahoo.co.jp>
2019-02-05 20:49:59 +0900

    validate article(title, content)

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190205111324) do
+ActiveRecord::Schema.define(version: 20190205113734) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "content"
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :article do
+		title { "MyTitle" }
     content { "MyText" }
     slug { "MyString" }
   end
